7d43a3297df444d897ff9d3c1a86bff0be780d22
adedolapo <shayne.doly94@gmail.com>
2021-11-19 19:08:00 +0100

    rubbish

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-//include required modules
-const jwt = require("jsonwebtoken")
+let request = require("request")
 const config = require("./config")
-const rp = require("request-promise")
-
+const jwt = require("jsonwebtoken")
 const express = require("express")
 const app = express()
 app.use(express.json())
@@ -18,59 +16,28 @@ const payload = {
 }
 const token = jwt.sign(payload, config.APISecret)
 
-//get the form
-app.get("/", (req, res) => res.send(req.body))
-
-//use userinfo from the form and make a post request to /userinfo
-app.post("/userinfo", (req, res) => {
-  //store the email address of the user in the email variable
-  email = req.body.email
-  //check if the email was stored in the console
-  console.log("email", email)
-  //Store the options for Zoom API which will be used to make an API call later.
-  var options = {
-    //You can use a different uri if you're making an API call to a different Zoom endpoint.
-    uri: "https://api.zoom.us/v2/users/" + email,
-    qs: {
-      status: "active",
-    },
-    auth: {
-      bearer: token,
-    },
+const getUser = callback => {
+  let options = {
+    method: "GET",
+    // A non-existing sample userId is used in the example below.
+    url: "https://api.zoom.us/v2/users/me",
     headers: {
-      "User-Agent": "Zoom-api-Jwt-Request",
-      "content-type": "application/json",
+      authorization: `Bearer ${token}`, // Do not publish or share your token publicly.
     },
-    json: true, //Parse the JSON string in the response
   }
 
-  //Use request-promise module's .then() method to make request calls.
-  rp(options)
-    .then(function (response) {
-      //printing the response on the console
-      console.log("User has", response)
-      //console.log(typeof response);
-      resp = response
-      //Adding html to the page
-      var title1 = "<center><h3>Your token: </h3></center>"
-      var result1 =
-        title1 +
-        '<code><pre style="background-color:#aef8f9;">' +
-        token +
-        "</pre></code>"
-      var title = "<center><h3>User's information:</h3></center>"
-      //Prettify the JSON format using pre tag and JSON.stringify
-      var result =
-        title +
-        '<code><pre style="background-color:#aef8f9;">' +
-        JSON.stringify(resp, null, 2) +
-        "</pre></code>"
-      res.send(result1 + "<br>" + result)
-    })
-    .catch(function (err) {
-      // API call failed...
-      console.log("API call failed, reason ", err)
-    })
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error)
+
+    // console.log(response)
+    return callback(body)
+  })
+}
+
+app.get("/home", async (req, res) => {
+  getUser(response => {
+    res.json({ tokenJwt: response })
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
